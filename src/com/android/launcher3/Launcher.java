ffa9985ba69a904dd2440bd2d9e5bdf19f65895f
maxwen <max.weninger@gmail.com>
2017-03-13 12:47:30 +0100

    Launcher3: allow hiding QSB
    Change-Id: I5fc3f162081563006d91eeb8368259ef7699f984

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -2903,6 +2903,7 @@ public class Launcher extends BaseActivity
             getWindow().getDecorView()
                     .sendAccessibilityEvent(AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED);
         }
+        mWorkspace.updateQsbVisibility();
         return changed;
     }
 
