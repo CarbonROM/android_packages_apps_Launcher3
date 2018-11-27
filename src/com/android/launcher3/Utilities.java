64df9477277c3943de1c0eb8ab93863ba3419060
Gábor Vass <hundeva@gmail.com>
2018-11-27 16:35:39 +0100

    Option to hide/show quicksearchbar
    Bootleggers (jenslody) edit: Let's toggle just the bottom search app, as the all apps search toggle doesn't seems to work.
    Original commit: https://github.com/hundeva/Lean-Launcher/commit/4a387a1ab285071a2fcd73f77f39e612a011f2c9
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Mrinal Kant Shukla <kamikaze.mrinal@gmail.com>
    Change-Id: Iaa363dd70c7283d9fc451402537993d77fa5907b

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -152,6 +152,7 @@ public final class Utilities {
 
     public static final String PREF_CUSTOM_RECENTS_ROUND_SWITCH = "pref_custom_recents_corner_switch";
     public static final String PREF_CUSTOM_RECENTS_ROUND_TYPE = "pref_custom_recents_corner_type";
+    public static final String BOTTOM_SEARCH_BAR_KEY = "pref_bottom_search_bar";
 
     public static boolean useNotificationsGesture(Context context) {
         return getPrefs(context).getBoolean(PREF_NOTIFICATIONS_GESTURE, true);
@@ -272,6 +273,10 @@ public final class Utilities {
         } catch (Exception e) {
             return preferenceFallback;
         }
+    }
+
+    public static boolean isBottomSearchBarVisible(Context context) {
+        return getPrefs(context).getBoolean(BOTTOM_SEARCH_BAR_KEY, true);
     }
 
     /**
