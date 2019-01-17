743adc54aaf8434107eaab8dc7d0089ed333568b
Alex Cruz <du.alexcruz@gmail.com>
2019-01-17 09:10:33 -0600

    Notification gesture: use our ActionUtils
    Change-Id: Iac027065351419f76008ed23e406362d6bae20c0

--- src/com/android/launcher3/Workspace.java
+++ src/com/android/launcher3/Workspace.java
@@ -311,7 +311,7 @@ public class Workspace extends PagedView<WorkspacePageIndicator>
                     if (e2.getY() - e1.getY() > 160/*min distance*/
                             && Math.abs(velocityY) > 250/*min speed*/) {
                         if(Utilities.useNotificationsGesture(context)) {
-                            openNotifications();
+                            ActionUtils.toggleNotifications();
                         }
                     }
                 } catch (Exception e) {
@@ -342,17 +342,6 @@ public class Workspace extends PagedView<WorkspacePageIndicator>
 
     public void setGestures(int mode) {
         mGestureMode = mode;
-    }
-
-    private boolean openNotifications() {
-        try {
-            Class.forName("android.app.StatusBarManager")
-                    .getMethod("expandNotificationsPanel")
-                    .invoke(mLauncher.getSystemService("statusbar"));
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
     }
 
     public boolean checkCustomGestures(MotionEvent ev) {
